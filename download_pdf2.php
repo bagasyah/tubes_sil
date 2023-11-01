aa04f8254bd60e2779ff5a7c61975123c2902acc
bagasyah <bagassyah18@gmail.com>
2023-11-01 18:14:35 +0700

    up

--- download_pdf2.php
+++ download_pdf2.php
@@ -26,7 +26,18 @@ if (isset($_GET['id'])) {
 }
 
 $result = $conn->query($query);
-
+function calculateBBM($jenis_perjalanan, $total_km)
+{
+    $bbm_per_km = 1 / 10;
+    $perkiraan_bbm = round($total_km * $bbm_per_km); // Bulatkan hasil jika koma
+    return $perkiraan_bbm;
+}
+function calculatebiaya($perkiraan_bbm)
+{
+    $harga = 14000;
+    $biaya = $perkiraan_bbm * $harga;
+    return $biaya;
+}
 if ($result->num_rows > 0) {
     if (isset($_GET['id'])) {
         // Mendownload satu dokumen laporan
@@ -100,62 +111,46 @@ if ($result->num_rows > 0) {
         // Buat objek PDF
         $pdf = new FPDF();
         $pdf->AddPage();
-
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(0, 10, 'Laporan Perjalanan', 0, 1, 'C');
+        $pdf->Ln();
         // Tulis data dari database
         $counter = 0;
         while ($row = $result->fetch_assoc()) {
-            $pdf->SetFont('Arial', 'B', 12);
-            $pdf->Cell(0, 10, 'Laporan Perjalanan', 0, 1, 'C');
-            $pdf->Ln();
+
 
             // ... Kode untuk menulis data laporan ...
 
             // Tabel utama
-            $pdf->SetFont('Arial', '', 10);
+            $pdf->SetFont('Arial', 'B', 10);
             $header = array('Nama', 'Tanggal', 'Alamat Awal', 'Alamat Tujuan', 'Jenis Perjalanan');
             foreach ($header as $col) {
                 $pdf->Cell(37, 10, $col, 1);
             }
             $pdf->Ln();
+            $pdf->SetFont('Arial', '', 10);
             $pdf->Cell(37, 10, $row['username'], 1);
             $pdf->Cell(37, 10, $row['tanggal'], 1);
             $pdf->Cell(37, 10, $row['alamat_awal'], 1);
             $pdf->Cell(37, 10, $row['alamat_tujuan'], 1);
             $pdf->Cell(37, 10, $row['jenis_perjalanan'], 1);
             $pdf->Ln();
-            $pdf->Ln();
             // Tabel KM awal, KM akhir, dan Total KM
             $pdf->SetFont('Arial', 'B', 10);
-            $pdf->SetFont('Arial', '', 10);
-            $header_km = array('KM Awal', 'KM Akhir', 'Total KM');
+            $header_km = array('KM Awal', 'KM Akhir', 'Total KM', 'Perkiraan BBM', 'Biaya');
             foreach ($header_km as $col) {
                 $pdf->Cell(37, 10, $col, 1);
             }
             $pdf->Ln();
-
-            $pdf->Cell(37, 10, $row['km_awal'], 1);
-            $pdf->Cell(37, 10, $row['km_akhir'], 1);
+            $pdf->SetFont('Arial', '', 10);
+            $pdf->Cell(37, 10, $row['km_awal'] . ' km', 1);
+            $pdf->Cell(37, 10, $row['km_akhir'] . ' km', 1);
             $total_km = $row['km_akhir'] - $row['km_awal'];
-            $pdf->Cell(37, 10, $total_km, 1);
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Ln();
+            $pdf->Cell(37, 10, $total_km . ' km', 1);
+            $perkiraan_bbm = calculateBBM($row['jenis_perjalanan'], $total_km);
+            $biaya = calculatebiaya($perkiraan_bbm);
+            $pdf->Cell(37, 10, $perkiraan_bbm . ' Liter', 1);
+            $pdf->Cell(37, 10, 'Rp.' . $biaya, 1);
             $pdf->Ln();
             $pdf->Ln();
             $pdf->Ln();
